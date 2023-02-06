932f1cae4b12b3a5decd3755e0c0e229ee20c3b4
Frankie Rodrigues <francis.n.rodrigues@gmail.com>
2023-02-05 20:09:18 -0500

    added tests

--- section2_my_attempt/blog.py
+++ section2_my_attempt/blog.py
@@ -3,9 +3,19 @@ from typing import Optional, List
 
 
 class Blog:
-    def __init__(self, title, author, Optional[List[]] = []):
+    def __init__(self, title, author):
         self.title = title
         self.author = author
         posts = [] or None
 
 
+    def __repr__(self) -> str:
+        plural_posts = 's' if len(self.posts) > 1 else ''
+        return f'{self.title} by {self.author} ({len(self.posts)} post{plural_posts})'
+
+
+    def create_post(self, title, content):
+        pass
+
+    def json(self):
+        pass
