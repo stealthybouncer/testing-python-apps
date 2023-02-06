095fe4ef5bb74d918535420ed16414248348af1c
Frankie Rodrigues <francis.n.rodrigues@gmail.com>
2023-02-05 20:09:39 -0500

    added tests

---
+++ section2_my_attempt/app.py
@@ -0,0 +1,4 @@
+from blog import Blog
+
+if __name__ == '__main__':
+    pass
--- section2_my_attempt/tests/unit/test_blog.py
+++ section2_my_attempt/tests/unit/test_blog.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from section2_my_attempt.blog import Blog
+from blog import Blog
 
 class BlogTest(TestCase):
     def test_create_blog(self):
@@ -7,3 +7,23 @@ class BlogTest(TestCase):
         
         self.assertEqual('Test', b.title)
         self.assertEqual('Test Author', b.author)
+        self.assertListEqual([], b.posts)
+
+    
+    def test_repr(self):
+        b = Blog('Test',  'Test Author')
+        b2 = Blog('My Day', 'Rolf')
+
+        self.assertEqual(b.__repr__(), 'Test by Test Author (0 posts)')
+        self.assertEqual(b2.__repr__(), 'My Day by Rolf (0 posts)')
+
+
+    def test_repr_multiple_posts(self):
+        b = Blog('Test',  'Test Author')
+        b.posts = ['test']
+        b2 = Blog('My Day', 'Rolf')
+        b2.posts = ['test', 'another']
+
+        self.assertEqual(b.__repr__(), 'Test by Test Author (1 posts)')
+        self.assertEqual(b2.__repr__(), 'My day by Rolf (2 posts)')
+        
